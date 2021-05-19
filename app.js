560e9b1089d8efe0626c5bde77467232113ae422
Sujata Regoti <sujata.regoti@gmail.com>
2021-05-19 15:11:47 +0530

    add signup endpoint

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('App is up');
+const users = [];
+
+app.use(express.json())
+
+app.post('/signup', (req, res) => {
+    const user = { ...req.body, id: users.length+1 };
+    users.push(user);
+    res.status(200).send({id: user.id});
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
