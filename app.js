4f46278ffa23ad8b2684f6f92ba620c07b3f50e6
Sujata Regoti <sujata.regoti@gmail.com>
2021-05-19 18:32:21 +0530

    Add login api

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const users = [];
+const MOBILE_NUMBER_REGEX = /^\d{10}$/;
+const users = {};
+let count = 1;
 
 app.use(express.json())
 
 app.post('/signup', (req, res) => {
-    const user = { ...req.body, id: users.length+1 };
-    users.push(user);
+    if (!MOBILE_NUMBER_REGEX.test(req.body.mobile_number)) {
+        return res.status(404).send({error: 'mobile number is not valid'})
+    }
+    if(users[req.body.mobile_number]) {
+        return res.status(404).send({error: 'user already registered'})
+    }
+    const user = { mobile_number: req.body.mobile_number, id: count };
+    users[user.mobile_number] = user;
+    count++;
     res.status(200).send({id: user.id});
+});
+
+app.post('/login', (req, res) => {
+    const user = users[req.body.mobile_number];
+    if(!user) {
+        return res.status(404).send({error: 'user not exists'})
+    }
+    res.status(200).send(user);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
